3fb9b9757ca6e4464ca7cb537feb3ee575f84628
Marcos Dolce <marcosdolce.91@gmail.com>
2023-09-19 23:43:55 -0300

    completo Dishes route

--- routes/dishes.js
+++ routes/dishes.js
@@ -2,11 +2,52 @@ var express = require('express');
 var router = express.Router();
 var { Dish } = require('../models/Dish');
 
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
+/* GET dishes listing. */
+router.get('/', async function (req, res, next) {
   try {
     const allDishes = await Dish.find();
     res.send(allDishes);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const dish = await Dish.findById(id);
+    res.send(dish);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newDish = new Dish({ ...req.body });
+    const insertedDish = await newDish.save();
+    res.send(insertedDish);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Dish.updateOne({ _id: id }, req.body);
+    const updatedDish = await Dish.findById(id);
+    res.send(updatedDish);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedDish = await Dish.findByIdAndDelete(id);
+    res.send(deletedDish);
   } catch (err) {
     next(err);
   }
